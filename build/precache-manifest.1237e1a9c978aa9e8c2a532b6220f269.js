21a119c4530fa744beaf96f26d41293872a0f849
RaviSriramChowdary <ravisriramchowdary@gmail.com>
2020-06-22 12:12:10 +0530

    Added .htaccess

---
+++ build/precache-manifest.1237e1a9c978aa9e8c2a532b6220f269.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5283eb1ea4a33b847b0a8c9ed712bbdc",
+    "url": "/one-ta-pp/index.html"
+  },
+  {
+    "revision": "8c09aa080134be3e4e2f",
+    "url": "/one-ta-pp/static/css/main.fd8d1c32.chunk.css"
+  },
+  {
+    "revision": "45c83990588a41e8282d",
+    "url": "/one-ta-pp/static/js/2.d955e815.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/one-ta-pp/static/js/2.d955e815.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c09aa080134be3e4e2f",
+    "url": "/one-ta-pp/static/js/main.3bc53b4b.chunk.js"
+  },
+  {
+    "revision": "c665f4d4bba22d244535",
+    "url": "/one-ta-pp/static/js/runtime-main.39906800.js"
+  },
+  {
+    "revision": "6798eea9daac18bf5c825735bd5097e2",
+    "url": "/one-ta-pp/static/media/algo.6798eea9.png"
+  },
+  {
+    "revision": "7f636e2ea2467f79116ef58a57231abb",
+    "url": "/one-ta-pp/static/media/bg.7f636e2e.png"
+  },
+  {
+    "revision": "8c0b82886160b34a9364e9fe54db8a4b",
+    "url": "/one-ta-pp/static/media/bg3.8c0b8288.jpg"
+  },
+  {
+    "revision": "3a92a3288d3823b144855e451b8f27e7",
+    "url": "/one-ta-pp/static/media/comparch.3a92a328.jpg"
+  },
+  {
+    "revision": "9b91f194606ea55b57030d5717fef7f2",
+    "url": "/one-ta-pp/static/media/dataAnalysis.9b91f194.png"
+  },
+  {
+    "revision": "9927f192c60b51ad765166351d8c558e",
+    "url": "/one-ta-pp/static/media/default.9927f192.jpg"
+  },
+  {
+    "revision": "e6e4cffbdcfb88937c3b151a6c102ee6",
+    "url": "/one-ta-pp/static/media/deregAnalysis.e6e4cffb.jpg"
+  },
+  {
+    "revision": "6fbcd4c6ed03817f45c0678cbe4ab7ae",
+    "url": "/one-ta-pp/static/media/deregAnalysis2.6fbcd4c6.jpg"
+  },
+  {
+    "revision": "a92d0f57d3c69f54ebec33994527aa39",
+    "url": "/one-ta-pp/static/media/devicemanagement.a92d0f57.png"
+  },
+  {
+    "revision": "9f9ea8c020131598f3d64f4b36e6b212",
+    "url": "/one-ta-pp/static/media/network.9f9ea8c0.png"
+  },
+  {
+    "revision": "066eb83cd76653ee4c4ac8df8fec7420",
+    "url": "/one-ta-pp/static/media/software.066eb83c.jpg"
+  },
+  {
+    "revision": "23fb8eb517d6c67e260b03150fa107d0",
+    "url": "/one-ta-pp/static/media/subjects.23fb8eb5.jpg"
+  }
+]);
